6890d2ab2aa72c0eace183fc63d03237adc77a97
Gabriel Sivieri Pistori <98199087+pistorigabriel@users.noreply.github.com>
2022-11-02 21:11:18 -0400

    Add files via upload

---
+++ ForcasArmadas.java
@@ -0,0 +1,79 @@
+abstract class Aeronautica{
+    private String nomeVeiculo;
+    private double custo;
+    private int poder;
+    void AtaqueAereo() {
+
+    }
+    String getInfo(){
+
+    }
+}
+final class Bombardeiros extends Aeronautica{
+    Bombardeiros(){
+        
+    }
+}
+final class Cargueiros extends Aeronautica{
+    Cargueiros(){
+
+    }
+}
+final class Jatos extends Aeronautica{
+    Jatos(){
+
+    }
+}
+
+abstract class Exercito {
+    private String nomeVeiculo;
+    private double custo;
+    private int poder;
+    void AtaqueTerrestre(){
+        
+    }
+    String getInfo(){
+
+    }
+}
+final class Blindados extends Exercito{
+    Blindados(){
+
+    }
+}
+final class Caminhoes extends Exercito{
+    Caminhoes(){
+
+    }
+}
+final class Tanques extends Exercito{
+    Tanques(){
+
+    }
+}
+abstract class Marinha{
+    private String nomeVeiculo;
+    private double custo;
+    private int poder;
+    void AtaqueMaritimo() {
+        
+    }
+    String getInfo(){
+
+    }
+}
+final class Navios extends Marinha{
+    Navios(){
+
+    }
+}
+final class PortaAvioes extends Marinha{
+    PortaAvioes(){
+
+    }
+}
+final class Submarinos extends Marinha{
+    Submarinos(){
+
+    }
+}   
