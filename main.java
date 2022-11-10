8b292ae6af0e7d82654167d0b944fbd15c1560ab
Gabriel Sivieri Pistori <98199087+pistorigabriel@users.noreply.github.com>
2022-11-10 09:32:48 -0400

    Update main.java

--- main.java
+++ main.java
@@ -6,34 +6,29 @@ public class main {
         Scanner scanf = new Scanner(System.in);
         System.out.println("Bem-vindo ao WorldWarRPG!");
         System.out.println("Aperte a tecla ENTER para começar.");
-        scanf.readLine();
         System.out.println("Bom, a primeira coisa que você precisa fazer é dar um nome ao seu novo Império.");
-        scanf.readLine();
         System.out.println("Como você deseja nomear essa gigante Aliança?");
         String nomeAlianca = scanf.next();
         System.out.println("Bela escolha! Agora que a aliança " + nomeAlianca + " já tem um líder, precisamos saber qual país servirá como sua sede.");
-        scanf.readLine();
         System.out.println("Primeiro de tudo, precisamos saber em qual continente você deseja que sua sede esteja localizada.");
-        scanf.readLine();
         System.out.println("1. África\n2. América\n3. Ásia\n4. Europa\n5. Oceania");
         System.out.println("Digite o número que corresponde ao seu continente de preferência.");
         int numContinente = scanf.nextInt();
         if (numContinente==1)
-            String SelContinente = África;
+            System.out.println("Ótimo, dentro da África temos os seguintes países:");
         else if (numContinente==2)
-            String SelContinente = América;
+            System.out.println("Ótimo, dentro da América temos os seguintes países:");
         else if (numContinente==3)
-            String SelContinente = Ásia;
+            System.out.println("Ótimo, dentro da Ásia temos os seguintes países:");
         else if (numContinente==4)
-            String SelContinente = Europa;
+            System.out.println("Ótimo, dentro da Europa temos os seguintes países:");
         else if (numContinente==5)
-            String SelContinente = Oceania;
-        System.out.println("Ótimo, dentro da " + SelContinente + " temos os seguintes países:");
+            System.out.println("Ótimo, dentro da Oceania temos os seguintes países:");
         for (Paises p : Paises.values())
             if (p.getContinente()==numContinente)
-                System.out.println("Nome: " + getNome() + " --- " + "Sigla: " + getSigla());
+                System.out.println("Nome: " + p.getNome() +  " (" + p.getSigla() + ")");
         System.out.println("Escolha seu país-sede digitando a sigla dele:");
         System.out.println("Dica: Pense na sua sede de forma estratégica, seja por localização, tamanho ou outros motivos");
-        }
+    }
 }
-}
+
