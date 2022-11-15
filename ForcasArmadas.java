9fd7ab68a7ea8091d30fd144c39d3cd894de5f93
Gabriel Sivieri Pistori <98199087+pistorigabriel@users.noreply.github.com>
2022-11-14 22:45:02 -0400

    Update ForcasArmadas.java

--- ForcasArmadas.java
+++ ForcasArmadas.java
@@ -1,80 +1,166 @@
-abstract class Aeronautica{
+//FORÇAS ARMADAS--------------------------------------------------------------------------------------------
+public abstract class ForcasArmadas{
     private String nomeVeiculo;
     private double custo;
     private int poder;
-    void AtaqueAereo(Paises p){
-        if (resultado==1)
-            adicionaPais(Paises p);
+    public String getNomeV(){
+        return nomeVeiculo;
     }
-    String getInfo(){
+    public double getCusto(){
+        return custo;
+    }
+    public int getPoder(){
+        return poder;
+    }
+    public abstract void imprimeInfo();
+    ForcasArmadas(String nome, double cost, int power){
+        this.nomeVeiculo = nome;
+        this.custo = cost;
+        this.poder = power;
+    }
+}
+//---------------------------------------------------------------------------------------------------------
 
+//AERONÁUTICA----------------------------------------------------------------------------------------------
+abstract class Aeronautica extends ForcasArmadas{
+    //public abstract void AtaqueAereo();
+    public Aeronautica(String nome, double cost, int power){
+        super(nome,cost,power);
     }
 }
 final class Bombardeiros extends Aeronautica{
-    Bombardeiros(){
-        
+    private int boostPoder;
+    public int getBoostP(){
+        return boostPoder;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / Boost: +" + getBoostP() + "% de Poder");
+    }
+    Bombardeiros(String nome, double cost, int power, int boost){
+        super(nome,cost,power);
+        this.boostPoder = boost;
     }
 }
 final class Cargueiros extends Aeronautica{
-    Cargueiros(){
-
+    private int Capacidade;
+    public int getCapacidade(){
+        return Capacidade;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / Capacidade: " + getCapacidade());
+    }
+    Cargueiros(String nome, double cost, int power, int capacity){
+        super(nome,cost,power);
+        this.Capacidade = capacity;
     }
 }
 final class Jatos extends Aeronautica{
-    Jatos(){
-
+    private int boostChances;
+    public int getBoostC(){
+        return boostChances;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / AumentoDeChance: +" + getBoostC() + "% de chances de êxito");
+    }
+    Jatos(String nome, double cost, int power, int chance){
+        super(nome,cost,power);
+        this.boostChances = chance;
     }
 }
+//---------------------------------------------------------------------------------------------------------
 
-abstract class Exercito {
-    private String nomeVeiculo;
-    private double custo;
-    private int poder;
-    void AtaqueTerrestre(){
-        
-    }
-    String getInfo(){
-
+//EXÉRCITO-------------------------------------------------------------------------------------------------
+abstract class Exercito extends ForcasArmadas {
+    //public abstract void AtaqueTerrestre();
+    public Exercito(String nome, double cost, int power){
+        super(nome,cost,power);
     }
 }
 final class Blindados extends Exercito{
-    Blindados(){
-
+    private int boostPoder;
+    public int getBoostP(){
+        return boostPoder;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / Boost: +" + getBoostP() + "% de Poder");
+    }
+    Blindados(String nome, double cost, int power, int boost){
+        super(nome,cost,power);
+        this.boostPoder = boost;
     }
 }
 final class Caminhoes extends Exercito{
-    Caminhoes(){
-
+    private int Capacidade;
+    public int getCapacidade(){
+        return Capacidade;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / Capacidade: " + getCapacidade());
+    }
+    Caminhoes(String nome, double cost, int power, int capacity){
+        super(nome,cost,power);
+        this.Capacidade = capacity;
     }
 }
 final class Tanques extends Exercito{
-    Tanques(){
-
+    private int boostChances;
+    public int getBoostC(){
+        return boostChances;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / AumentoDeChance: +" + getBoostC() + "% de chances de êxito");
+    }
+    Tanques(String nome, double cost, int power, int chance){
+        super(nome,cost,power);
+        this.boostChances = chance;
     }
 }
-abstract class Marinha{
-    private String nomeVeiculo;
-    private double custo;
-    private int poder;
-    void AtaqueMaritimo() {
-        
-    }
-    String getInfo(){
+//---------------------------------------------------------------------------------------------------------
 
+//MARINHA--------------------------------------------------------------------------------------------------
+abstract class Marinha extends ForcasArmadas{
+    //public abstract void AtaqueMaritimo();
+    public Marinha(String nome, double cost, int power){
+        super(nome,cost,power);
     }
 }
 final class Navios extends Marinha{
-    Navios(){
-
+    private int boostPoder;
+    public int getBoostP(){
+        return boostPoder;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / Boost: +" + getBoostP() + "% de Poder");
+    }
+    Navios(String nome, double cost, int power, int boost){
+        super(nome,cost,power);
+        this.boostPoder = boost;
     }
 }
 final class PortaAvioes extends Marinha{
-    PortaAvioes(){
-
+    private int Capacidade;
+    public int getCapacidade(){
+        return Capacidade;
+    }
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / Capacidade: " + getCapacidade());
+    }
+    PortaAvioes(String nome, double cost, int power, int capacity){
+        super(nome,cost,power);
+        this.Capacidade = capacity;
     }
 }
 final class Submarinos extends Marinha{
-    Submarinos(){
-
+    private int boostChances;
+    public int getBoostC(){
+        return boostChances;
     }
-}   
+    public void imprimeInfo(){
+        System.out.println("Nome: " + getNomeV() + " / Custo: $" + getCusto() + " / Poder: " + getPoder() + " / AumentoDeChance: +" + getBoostC() + "% de chances de êxito");
+    }
+    Submarinos(String nome, double cost, int power, int chance){
+        super(nome,cost,power);
+        this.boostChances = chance;
+    }
+}
+//---------------------------------------------------------------------------------------------------------   
